89a796282fe4e463627b45076f0a37751912e8df
Aigul Khafizova <haku.kavai@gmail.com>
2017-12-26 21:51:03 +0300

    Update server

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -20,27 +21,31 @@ var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'pokemon
 var session = driver.session();
 
 app.get('/', function(req, res){
-    session
-        .run('CREATE (a:Point {id: 1, x: 4, y: 345}) RETURN a')
-        .then(function(result){
-            return res.render('view');
-        })
-        .catch(function(err){
-            console.log(err);
-        });
+    return res.render('start');    
 });
+
+app.post('/path/add', function(req, res){
+    var pA = req.body.pointA;
+    var pB = req.body.pointB;
+    console.log(pA);
+    return res.render('view', {pointA: pA, pointB:pB,complexity: 1});   
+    
+     res.redirect('/');
+})
 
 app.post('/coord/add', function(req, res){
     var idStr = req.body.coord_id;
     var xStr = req.body.coord_x;
     var yStr = req.body.coord_y;
     var strStr = req.body.coord_str;
+    var A=req.body.pA; 
+    var B=req.body.pB; 
 
     var arrId = idStr.split(' ');
     var arrX = xStr.split(' ');
     var arrY = yStr.split(' ');
     var arrStr = strStr.split(' , ');
-    var compl = 1;
+    var compl = (arrId.length-2);
 
     function newDot(id, x, y, str){
         session
@@ -51,40 +56,25 @@ app.post('/coord/add', function(req, res){
             result.records.forEach(function(record){
                 console.log(record._fields[0].properties);
             })
-            // return res.render('view', {complexity: compl});
+             //return res.render('view');
         })
         .catch(function(err){
             console.log(err);
         });  
     } 
 
-    function newRelation(id1, id2){
-        session
-        .run("MATCH (a:Dot),(b:Dot) WHERE a.id = {id1} AND b.id = {id2}",
-            "CREATE (a)-[r:then]->(b)",
-            "RETURN r")
-        .then(function(result){
-            // result.records.forEach(function(record){
-            //     console.log(record._fields[0].properties);
-            // })
-            // return res.render('view', {complexity: compl});
-        })
-        .catch(function(err){
-            console.log(err);
-        });  
-    }
-
     for (var i=0; i<arrId.length-1; i++)
     {
         newDot(arrId[i], arrX[i], arrY[i], arrStr[i]);  
     }
-
-    // for (var i=0; i<arrId.length-2; i++){
-    //     newRelation(i, i+1);
-    // }
+    function getCompl(count){
+        return (count-2);
+    };
+    return res.render('view', {pointA: A, pointB: B, complexity: compl});
+    console.log(getCompl(arrId.length));
     
     
-    res.redirect('/');
+     res.redirect('/');
 })
 
 app.listen(3000);
